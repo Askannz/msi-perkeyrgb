f4bf0fb7682b0bc7ee7b8c5b21ed2517ab2ec9ff
Saif Addin <saif@johnsnowlabs.com>
2019-03-29 10:20:23 -0700

    Small script to randomize presets or disable them
    Just a tiny proposal to a script to be mapped to keyboard

---
+++ switch_rgb.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+# Randomly sets a new preset
+# 1st arg is model name. 2nd argument -d to disable.
+
+if [ "$2" = "-d" ]
+then
+  echo "rgb disabled"
+  msi-perkeyrgb -m $1 $2
+  exit 0
+fi
+
+presets=(
+    aqua
+    chakra
+    default
+    disco
+    drain
+    freeway
+    plain
+    rainbow-split
+    roulette
+)
+
+selection=${presets[((RANDOM % 10+1))]}
