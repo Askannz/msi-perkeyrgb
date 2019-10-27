490a0928d06a138d23767f96c7948c7dc7b3442c
Robin Lange <robin.langenc@gmail.com>
2019-10-27 20:07:51 +1100

    Release v2.0

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 from os.path import dirname, join
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='msi-perkeyrgb',
-    version='1.4-effect-alpha',
+    version='2.0',
     description='Configuration tool for per-key RGB keyboards on MSI laptops.',
     long_description=open(
         join(dirname(__file__), 'README.md')).read(),
@@ -13,13 +13,13 @@ setup(
     author='Robin Lange',
     author_email='robin.langenc@gmail.com',
     license='MIT',
-    packages=['msi_perkeyrgb'],
+    packages=find_packages(),
     entry_points={
         'console_scripts': [
             'msi-perkeyrgb=msi_perkeyrgb.main:main',
         ],
     },
-    package_data={'msi_perkeyrgb': ['presets/*.json']},
+    package_data={'msi_perkeyrgb': ['protocol_data/presets/*.json']},
     keywords=['msi', 'rgb', 'keyboard', 'per-key'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
